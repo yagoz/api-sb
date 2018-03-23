fab7cec0df2492ce48aec397f4e97b46b47ac9f8
Yago <yzerbarini@qubit.tv>
2018-03-22 21:45:07 -0300

    Change site title

--- views/partials/head.php
+++ views/partials/head.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Hello Bulma!</title>
+    <title>Scatchbling - 2018</title>
     <link rel="stylesheet" href="https://jenil.github.io/bulmaswatch/pulse/bulmaswatch.min.css">
     <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
   </head>
